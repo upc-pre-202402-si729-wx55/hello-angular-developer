2f62c7882502a3ca69df15ee58a89d88a7454773
Angel Velasquez <pcisavel@upc.edu.pe>
2024-08-28 12:24:08 -0500

    feat(main): implemented main behavior.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {RegisterDeveloperComponent} from "./welcoming/components/register-developer/register-developer.component";
+import {WelcomeComponent} from "./welcoming/components/welcome/welcome.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegisterDeveloperComponent, WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hello-angular-developer';
+  firstName: string = 'Unknown';
+  lastName: string = '';
+
+  onDeveloperRegistered(developer: { firstName: string, lastName: string }) {
+    this.firstName = developer.firstName;
+    this.lastName = developer.lastName;
+  }
 }
